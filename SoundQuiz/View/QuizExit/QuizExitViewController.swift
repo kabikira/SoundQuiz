ede2216b0d314ac1c5722727370d55bede1064d1
kabikira <kabikirar2@protonmail.com>
2023-04-01 20:46:49 +0900

    Lotti追加

--- SoundQuiz/View/QuizExit/QuizExitViewController.swift
+++ SoundQuiz/View/QuizExit/QuizExitViewController.swift
@@ -6,11 +6,17 @@
 //
 
 import UIKit
+import Lottie
 
 class QuizExitViewController: UIViewController {
     
     @IBOutlet var titleLabel: UILabel!
     @IBOutlet var continueButton: UIButton!
+    
+    @IBOutlet weak var animationContainerView1: UIView!
+    private var animationView1 = LottieAnimationView()
+    
+    
     static func makeFromStoryboard(scoreCountModel: ScoreCountModel) -> QuizExitViewController {
             let vc = UIStoryboard(name: "QuizExit", bundle: nil).instantiateInitialViewController() as! QuizExitViewController
         vc.scoreCountModel = scoreCountModel
@@ -20,7 +26,9 @@ class QuizExitViewController: UIViewController {
     private var scoreCountModel = ScoreCountModel(scoreCount: 0)
     // いらんかもメモリ食うからリセットした方がいいかな
     @IBAction func continueGame(_ sender: Any) {
-        Router.shared.showReStart()
+//        Router.shared.showReStart()
+        
+
     }
     
 
@@ -29,8 +37,25 @@ class QuizExitViewController: UIViewController {
         super.viewDidLoad()
         self.navigationItem.hidesBackButton = true
         titleLabel.text = "\(scoreCountModel.scoreCount.description)点/100点"
+        
+        guard let animation = LottieAnimation.named("4329-confetti", subdirectory: nil)
+        else {
+            print("\(#line) file not found")
+            return
+        }
+        animationContainerView1.addSubview(animationView1)
+        animationView1.translatesAutoresizingMaskIntoConstraints = false
 
-        // Do any additional setup after loading the view.
+        NSLayoutConstraint.activate([
+                animationView1.heightAnchor.constraint(equalTo: animationContainerView1.heightAnchor),
+                animationView1.leadingAnchor.constraint(equalTo: animationContainerView1.leadingAnchor),
+                animationView1.trailingAnchor.constraint(equalTo: animationContainerView1.trailingAnchor),
+                animationView1.bottomAnchor.constraint(equalTo: animationContainerView1.bottomAnchor),
+            ])
+        animationView1.contentMode = .scaleAspectFit
+        animationView1.animation = animation
+        animationView1.play(fromProgress: 0, toProgress: 1, loopMode: .loop)
+       
     }
     
 
